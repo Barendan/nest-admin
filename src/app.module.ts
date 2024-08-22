c8232f0e965be22eaf831551e568e071bce19c42
Barendan <barendan@gmail.com>
2024-08-22 16:38:02 -0400

    clean up app module, container, services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { RoleModule } from './role/role.module';
@@ -26,10 +24,7 @@ import { PermissionModule } from './permission/permission.module';
     CommonModule,
     RoleModule,
     PermissionModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  ]
 })
-
 
 export class AppModule {}
